9281db35272173be07067939b6145844927d9f3c
eddxw <eduardo.jemb@gmail.com>
2018-12-27 08:51:44 -0700

    setup home and about pages routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/homepage'
-  get 'pages/about'
+  root 'pages#homepage'
+  get 'about', to: 'pages#about' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
