18a532d052273887cf7b8e6da613abe3d8ef0dc5
eddxw <eduardo.jemb@gmail.com>
2018-12-28 21:29:07 -0700

    edit update articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,15 +2,30 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
-    
+
     if @article.save
-      flash[:notice] = "Article was succesfully created"
+      flash[:notice] = 'Article was succesfully created'
       redirect_to article_path(@article)
     else
       flash[:notice] = @article.errors.full_messages
-      render "new"
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was succesfully updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
   end
 
@@ -19,6 +34,7 @@ class ArticlesController < ApplicationController
   end
 
   private
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
