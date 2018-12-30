c8a85b569282be8742f01231cca1078235662464
eddxw <eduardo.jemb@gmail.com>
2018-12-30 12:18:45 -0700

    Sign up users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   resources :articles
 
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
   mount Peek::Railtie => '/peek'
   root to: 'home#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
